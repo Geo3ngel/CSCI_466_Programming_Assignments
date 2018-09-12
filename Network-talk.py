c4b5ed6dd354edc40a0b2264d455c23be2baa303
Beau <bamemboy@gmail.com>
2018-09-12 10:01:52 -0600

    And again...

--- Network-talk.py
+++ Network-talk.py
@@ -2,4 +2,9 @@
 # Created by: George Engel, , &
 # 9/12/2018
 
-print("Hello World")
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='path/to/localhost.pem', server_side=True)
+httpd.serve_forever()
