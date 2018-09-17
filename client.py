fdc2c698f96b1292505ac3ea89038521965ef22f
George <GeoEngel.z@gmail.com>
2018-09-16 20:46:57 -0600

    Added other stuff

--- client.py
+++ client.py
@@ -1,2 +1,13 @@
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+
+r1 = conn.getresponse()
+
+print(r1.status, r1.reason)
+
+data1 = r1.read()  # This will return entire content.
+# The following example demonstrates reading data in chunks.
+
+conn.request("GET", "/")
+r1 = conn.getresponse()
